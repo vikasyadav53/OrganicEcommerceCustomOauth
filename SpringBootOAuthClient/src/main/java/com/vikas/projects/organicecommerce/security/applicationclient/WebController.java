39538f5171780e1a6c545a18a2f655a4ee052c66
Vikas Yadav <vikasyadav.a079@gmail.com>
2020-05-17 23:31:36 +0530

    Spring Boot Oauth working example

---
+++ SpringBootOAuthClient/src/main/java/com/vikas/projects/organicecommerce/security/applicationclient/WebController.java
@@ -0,0 +1,20 @@
+package com.vikas.projects.organicecommerce.security.applicationclient;
+
+import java.security.Principal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+
+	@RequestMapping("/securedPage")
+    public String securedPage(Model model, Principal principal) {
+        return "securedPage";
+    }
+    @RequestMapping("/")
+    public String index(Model model, Principal principal) {
+        return "index";
+    }
+}
---
+++ SpringBootOAuthClient/src/test/java/com/vikas/projects/organicecommerce/security/applicationclient/SpringBootOAuthClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikas.projects.organicecommerce.security.applicationclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOAuthClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
